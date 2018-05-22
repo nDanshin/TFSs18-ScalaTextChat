e97634f3800253cfa600bada19c6a29fad4a8341
nDanshin <danshin56@gmail.com>
2018-05-22 14:12:13 +0300

    now exactly added generated code

---
+++ generated_code/src/main/scala/datamodel/latest/schema/schema.scala
@@ -0,0 +1,46 @@
+package datamodel.latest.schema
+// AUTO-GENERATED Slick data model
+/** Stand-alone Slick data model for immediate use */
+object tables extends {
+  val profile = slick.jdbc.H2Profile
+} with tables
+
+/** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
+trait tables {
+  val profile: slick.jdbc.JdbcProfile
+  import profile.api._
+  import slick.model.ForeignKeyAction
+  // NOTE: GetResult mappers for plain SQL are only generated for tables where Slick knows how to map the types of all columns.
+  import slick.jdbc.{GetResult => GR}
+
+  /** DDL for all tables. Call .create to execute. */
+  lazy val schema: profile.SchemaDescription = Messages.schema
+  @deprecated("Use .schema instead of .ddl", "3.0")
+  def ddl = schema
+
+  /** Entity class storing rows of table Messages
+   *  @param id Database column ID SqlType(INTEGER)
+   *  @param data Database column DATA SqlType(VARCHAR) */
+  case class MessagesRow(id: Int, data: String)
+  /** GetResult implicit for fetching MessagesRow objects using plain SQL queries */
+  implicit def GetResultMessagesRow(implicit e0: GR[Int], e1: GR[String]): GR[MessagesRow] = GR{
+    prs => import prs._
+    MessagesRow.tupled((<<[Int], <<[String]))
+  }
+  /** Table description of table MESSAGES. Objects of this class serve as prototypes for rows in queries. */
+  class Messages(_tableTag: Tag) extends profile.api.Table[MessagesRow](_tableTag, "MESSAGES") {
+    def * = (id, data) <> (MessagesRow.tupled, MessagesRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(id), Rep.Some(data)).shaped.<>({r=>import r._; _1.map(_=> MessagesRow.tupled((_1.get, _2.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column ID SqlType(INTEGER) */
+    val id: Rep[Int] = column[Int]("ID")
+    /** Database column DATA SqlType(VARCHAR) */
+    val data: Rep[String] = column[String]("DATA")
+  }
+  /** Collection-like TableQuery object for table Messages */
+  lazy val Messages = new TableQuery(tag => new Messages(tag))
+}
+object Version{
+  def version = 2
+}
